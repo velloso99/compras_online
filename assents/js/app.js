9b42a9c3a5e1c31b9b5a10d5bea63b1b12b6077e
Rodrigo Lopes Velloso <velloso.rl9@outlook.com>
2025-04-14 18:53:34 -0300

    assents

--- assents/js/app.js
+++ assents/js/app.js
@@ -7,6 +7,21 @@ function menucelular() {
         menuitens.style.maxHeight ="200px";
     }else{
         menuitens.style.maxHeight ="0px";
-    }
-    
+    } 
+}
+
+var produtoimg = document.getElementById("produtoimg");
+var produtoMiniatura = document.getElementsByClassName("produtoMiniatura");
+
+produtoMiniatura[0].onclick = function(){
+    produtoimg.src = produtoMiniatura[0].src;
+}
+produtoMiniatura[1].onclick = function(){
+    produtoimg.src = produtoMiniatura[1].src;
+}
+produtoMiniatura[2].onclick = function(){
+    produtoimg.src = produtoMiniatura[2].src;
+}
+produtoMiniatura[3].onclick = function(){
+    produtoimg.src = produtoMiniatura[3].src;
 }
